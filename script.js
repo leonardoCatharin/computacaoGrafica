bc180b30a82a48f6494f6f1919231c4f4acab363
leonardoCatharin <leo_catharin@hotmail.com>
2017-01-23 15:08:58 -0200

    Criado o layout da ferramenta de desenho 2D

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-/**
- * Created by leo on 21/01/17.
- */
+function limpaCanvas() {
+    var altera_canvas = document.getElementById("canvas");
+    altera_canvas.width = altera_canvas.width;
+}
+
+function criaLinha(){
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    context.fillRect(50, 25, 150, 1);
+    context.stroke();
+}
